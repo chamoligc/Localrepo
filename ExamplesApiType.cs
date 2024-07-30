66373addd845502524c943ed4b9321574af8c89a
gaurav <chamoli.gaurav@gmail.com>
2024-07-30 15:32:42 +0530

    Initial commit from local system

---
+++ ExamplesApiType.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum ExamplesApiType
+    {
+        /// <summary>
+        /// Rooms API
+        /// </summary>
+        [Description("reg")]
+        Rooms = 0,
+
+        /// <summary>
+        /// ESignature API
+        /// </summary>
+        [Description("eg")]
+        ESignature = 1,
+
+        /// <summary>
+        /// Click API
+        /// </summary>
+        [Description("ceg")]
+        Click = 2,
+
+        /// <summary>
+        /// Monitor API
+        /// </summary>
+        [Description("meg")]
+        Monitor = 3,
+
+        /// <summary>
+        /// Admin API
+        /// </summary>
+        [Description("aeg")]
+        Admin = 4,
+
+        /// <summary>
+        /// Connect API
+        /// </summary>
+        [Description("con")]
+        Connect = 5,
+
+        /// <summary>
+        /// Web Forms API
+        /// </summary>
+        [Description("web")]
+        WebForms = 6,
+    }
+
+    public static class ExamplesApiTypeExtensions
+    {
+        public static string ToKeywordString(this ExamplesApiType val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val
+               .GetType()
+               .GetField(val.ToString())
+               .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
+    }
+}
---
+++ JwtAuth.cs
@@ -0,0 +1,92 @@
+﻿using DocuSign.eSign.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DocuSign.eSign.Client.Auth.OAuth;
+
+namespace ConsoleApp1
+{
+    public static class JwtAuth
+    {
+        public static OAuthToken AuthenticateWithJwt(string api, string clientId, string impersonatedUserId, string authServer, byte[] privateKeyBytes)
+        {
+            var docuSignClient = new DocuSignClient();
+            //var apiType = Enum.Parse<ExamplesApiType>(api);
+            var scopes = new List<string>
+                {
+                    "signature",
+                    "impersonation",
+                };
+            //if (apiType == ExamplesApiType.Rooms)
+            //{
+            //    scopes.AddRange(new List<string>
+            //    {
+            //        "dtr.rooms.read",
+            //        "dtr.rooms.write",
+            //        "dtr.documents.read",
+            //        "dtr.documents.write",
+            //        "dtr.profile.read",
+            //        "dtr.profile.write",
+            //        "dtr.company.read",
+            //        "dtr.company.write",
+            //        "room_forms",
+            //    });
+            //}
+
+            //if (apiType == ExamplesApiType.Click)
+            //{
+            //    scopes.AddRange(new List<string>
+            //    {
+            //        "click.manage",
+            //        "click.send",
+            //    });
+            //}
+
+            //if (apiType == ExamplesApiType.Monitor)
+            //{
+            //    scopes.AddRange(new List<string>
+            //    {
+            //        "signature",
+            //        "impersonation",
+            //    });
+            //}
+
+            //if (apiType == ExamplesApiType.Admin)
+            //{
+            //    scopes.AddRange(new List<string>
+            //    {
+            //        "user_read",
+            //        "user_write",
+            //        "account_read",
+            //        "organization_read",
+            //        "group_read",
+            //        "permission_read",
+            //        "identity_provider_read",
+            //        "domain_read",
+            //        "user_data_redact",
+            //        "asset_group_account_read",
+            //        "asset_group_account_clone_write",
+            //        "asset_group_account_clone_read",
+            //    });
+            //}
+
+            //if (apiType == ExamplesApiType.WebForms)
+            //{
+            //    scopes.Add("webforms_read");
+            //    scopes.Add("webforms_instance_write");
+            //    scopes.Add("webforms_instance_read");
+            //}
+
+            return docuSignClient.RequestJWTUserToken(
+                clientId,
+                impersonatedUserId,
+                authServer,
+                privateKeyBytes,
+                1,
+                scopes);
+
+        }
+    }
+}
